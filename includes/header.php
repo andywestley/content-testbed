78f528c17db8e30976a0f9bdd93eb14bb1c1bb22
andywestley <andy@andrewwestley.co.uk>
2026-05-27 14:40:35 +0100

    Add CSS cache buster to header to force WCAG color updates

--- includes/header.php
+++ includes/header.php
@@ -38,7 +38,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom CSS -->
-    <link href="/assets/css/style.css" rel="stylesheet">
+    <link href="/assets/css/style.css?v=2" rel="stylesheet">
 </head>
 <body>
     <header>
