f1c28ff059a29b495651b725950f24c5f22693b7
andywestley <andy@andrewwestley.co.uk>
2026-06-17 13:21:09 +0100

    Add passive voice content to news-bad-2.php to trigger active/passive warning

--- news-bad-2.php
+++ news-bad-2.php
@@ -7,7 +7,7 @@ include 'includes/header.php';
     <div class="col-lg-8">
         <div class="bad-content-notice">
             <h4>Intended Content Violations:</h4>
-            <p>Intensifiers ("very", "extremely", "actually"), weasel words ("basically", "some people say"), word overuse (repeating "laptop" constantly), rambling length.</p>
+            <p>Passive voice, intensifiers ("very", "extremely", "actually"), weasel words ("basically", "some people say"), word overuse (repeating "laptop" constantly), rambling length.</p>
         </div>
     </div>
 </div>
@@ -22,10 +22,10 @@ include 'includes/header.php';
             </div>
             
             <div itemprop="articleBody" class="fs-5">
-                <p>This tech company basically just launched a very, very fast laptop. The laptop is actually extremely impressive. Some people say that this laptop is the best laptop that has ever been made as a laptop. The laptop features a very strong battery that basically lasts a really long time, which is very good for laptop users. If you are a person who likes to use a laptop, this laptop is very clearly the laptop you need.</p>
-                <p>We actually think this laptop is basically a game-changer for the laptop market. The laptop screen is very bright and the laptop keyboard is extremely comfortable. If you use a laptop, this laptop will basically change how you use a laptop every single day. Many experts generally agree that the laptop is actually quite revolutionary, but some people might disagree about the laptop's overall laptop design.</p>
-                <p>It was revealed by the laptop makers that the laptop is actually quite cheap for a laptop. The laptop will be available very soon in stores, and people are extremely excited to buy the laptop. Basically, you should get this laptop if you need a laptop. A lot of consumers are actually planning to buy the laptop because the laptop is very affordable for a laptop.</p>
-                <p>In conclusion, the laptop is very fast, the laptop is very cheap, and the laptop is extremely good. We really, truly believe that this laptop will basically dominate the laptop industry. Actually, you should probably just go buy the laptop right now because the laptop is so very incredible.</p>
+                <p>This tech company basically just launched a very, very fast laptop. The laptop was designed by their top engineers to be extremely impressive. Some people say that this laptop is the best laptop that has ever been made as a laptop. The laptop features a very strong battery that basically lasts a really long time, which is very good for laptop users. If you are a person who likes to use a laptop, this laptop is very clearly the laptop you need.</p>
+                <p>We actually think this laptop is basically a game-changer for the laptop market. The laptop screen was engineered by a display team to be very bright, and the laptop keyboard was constructed by the manufacturer to be extremely comfortable. If you use a laptop, this laptop will basically change how you use a laptop every single day. Many experts generally agree that the laptop is actually quite revolutionary, but some people might disagree about the laptop's overall laptop design.</p>
+                <p>It was revealed by the laptop makers that the laptop is actually quite cheap for a laptop. The laptop will be distributed by local retailers very soon in stores, and people are extremely excited to buy the laptop. Basically, you should get this laptop if you need a laptop. A lot of consumers are actually planning to buy the laptop because the laptop is very affordable for a laptop.</p>
+                <p>In conclusion, the laptop was built by the company to be very fast, the laptop was priced by executives to be very cheap, and the laptop was rated by critics to be extremely good. We really, truly believe that this laptop will basically dominate the laptop industry. Actually, you should probably just go buy the laptop right now because the laptop is so very incredible.</p>
             </div>
         </div>
     </div>
